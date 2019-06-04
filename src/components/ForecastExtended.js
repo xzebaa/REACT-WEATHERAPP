9ed2115d492c009fe541764fcb28c6004c11b5fa
Sebastian <sealvarezlazo@gmail.com>
2019-06-04 00:38:48 -0400

    wip

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,12 +1,25 @@
 import React,{Component} from 'react';
+import ForecastItem from './ForecastItem';
+import './styles.css';
 
+const days = ['Lunes', 'Martes','Miercoles','Jueves','Viernes']
 class ForecastExtended extends Component {
+
+  renderForecastItemDays () {
+
+    return days.map( ( day ) => {
+      return (<ForecastItem weekDay={day}></ForecastItem>);
+    })
+    
+  }
 
     render () {
 
         const { city } =this.props
         return (
-            <div>{`Pronostico Extendido ${city}`}</div>
+            <div><h2 className='forecast-title'>{`Pronóstico Extendido ${city}`}</h2>
+            {this.renderForecastItemDays()}
+            </div>
         )
     }
 }
