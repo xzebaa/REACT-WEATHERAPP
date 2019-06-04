b89d5dd2ebaf7ef077f7888f338f7e91be44e733
sebastian <sealvarezlazo@gmail.com>
2019-06-04 17:56:38 -0400

    wip

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,27 +1,39 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import ForecastItem from './ForecastItem';
 import './styles.css';
 
-const days = ['Lunes', 'Martes','Miercoles','Jueves','Viernes']
+const days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes']
 class ForecastExtended extends Component {
 
-  renderForecastItemDays () {
-
-    return days.map( ( day ) => {
-      return (<ForecastItem weekDay={day}></ForecastItem>);
-    })
-    
+  constructor() {
+    super();
+    this.state = {
+      forecastData: null
+    };
   }
 
-    render () {
+  renderForecastItemDays() {
 
-        const { city } =this.props
-        return (
-            <div><h2 className='forecast-title'>{`Pronóstico Extendido ${city}`}</h2>
-            {this.renderForecastItemDays()}
-            </div>
-        )
-    }
+    return days.map((day) => {
+      return (<ForecastItem weekDay={day} hour={10}></ForecastItem>);
+    })
+
+  }
+
+  renderProgress = () => {
+    return <h3>Cargando contenido</h3>;
+  }
+
+  render() {
+
+    const { city } = this.props
+    const { forecastData } = this.props;
+    return (
+      <div><h2 className='forecast-title'>{`Pronóstico Extendido ${city}`}</h2>
+        {forecastData ? this.renderForecastItemDays() : this.renderProgress()}
+      </div>
+    )
+  }
 }
 
 export default ForecastExtended;
--- src/components/ForecastItem/index.js
+++ src/components/ForecastItem/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import WeatherData from './../WeatherLocation/WeatherData'
 
-const ForecastItem = ({weekDay}) => (
-  <div>{weekDay}</div>
+const data = {
+  temperature: 10,
+  humidity: 10,
+  weatherState: 'normal',
+  wind: 'normal',
+}
+
+const ForecastItem = ({ weekDay, hour }) => (
+  <div>
+    <div>{weekDay} hora: {hour} hr</div>
+    <WeatherData data={data}></WeatherData>
+  </div>
+
 );
 
 ForecastItem.propTypes = {
-  weekDay: PropTypes.string.isRequired
+  weekDay: PropTypes.string.isRequired,
+  hour: PropTypes.number.isRequired,
 }
 
 export default ForecastItem;
